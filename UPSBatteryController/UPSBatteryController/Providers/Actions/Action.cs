f876b4fadfd611059ec2843c43774917483fce95
shoborov <djatlik@mail.ru>
2017-11-10 16:57:52 +0300

    Добавлены функции добавления, удаления, и отредактирован внешний вид списка действий

--- UPSBatteryController/UPSBatteryController/Providers/Actions/Action.cs
+++ UPSBatteryController/UPSBatteryController/Providers/Actions/Action.cs
@@ -8,6 +8,16 @@ namespace UPSBatteryController.Providers.Actions
 {
     public class Action
     {
+        #region Constants
+
+        public const string ShutdownProgramm = "shutdown.exe";
+        public const string ShutdownArguments = "/s /t 0";
+        public const string ShutdownWorkingDir = @"C:\Windows\System32";
+
+        #endregion
+
+        #region Properties
+
         /// <summary>
         /// Идентификатор
         /// </summary>
@@ -23,13 +33,35 @@ namespace UPSBatteryController.Providers.Actions
         /// </summary>
         public ActionType Type { get; set; }
 
+        /// <summary>
+        /// Программа
+        /// </summary>
+        public string Programm { get; set; }
+
+        /// <summary>
+        /// Аргументы
+        /// </summary>
+        public string Arguments { get; set; }
+
+        /// <summary>
+        /// Рабочая папка
+        /// </summary>
+        public string WorkingDir { get; set; }
+
+        #endregion
+
+        #region Functions
+
         public Action Clone()
         {
             return new Action()
             {
                 Id = Id,
                 BatteryLevel = BatteryLevel,
-                Type = Type
+                Type = Type,
+                Programm = Programm,
+                Arguments = Arguments,
+                WorkingDir = WorkingDir
             };
         }
 
@@ -39,8 +71,13 @@ namespace UPSBatteryController.Providers.Actions
             {
                 Id = Guid.NewGuid(),
                 BatteryLevel = 25,
-                Type = ActionType.Shutdown
+                Type = ActionType.Shutdown,
+                Programm = ShutdownProgramm,
+                Arguments = ShutdownArguments,
+                WorkingDir = ShutdownWorkingDir
             };
         }
+
+        #endregion
     }
 }
